98ef4faf430cecb5fcc38e925e7196fd7d28a742
kk-no <kenokohe@gmail.com>
2025-05-10 16:35:16 +0900

    A version that has been confirmed to work on WASI runtimes

--- script/wasi_build.sh
+++ script/wasi_build.sh
@@ -15,4 +15,4 @@ make -j$CORES tournament \
   YANEURAOU_EDITION=YANEURAOU_ENGINE_NNUE \
   TARGET=../build/wasi/yaneuraou.wasm \
   CPPFLAGS="--target=wasm32-wasi-threads -pthread -matomics -mbulk-memory -ftls-model=local-exec -fno-exceptions -fno-rtti -DUSE_WASI -D__wasm32__ -DYANEURAOU_ENGINE_NNUE -DNO_EXCEPTIONS" \
-  LDFLAGS="-Wl,--export-memory -Wl,--shared-memory -Wl,--initial-memory=268435456 -Wl,--max-memory=4294967296 -Wl,--export-dynamic -Wl,--export=__heap_base -Wl,--export=__data_end"
+  LDFLAGS="-Wl,--export-memory -Wl,--shared-memory -Wl,--initial-memory=536870912 -Wl,--max-memory=4294967296 -Wl,--export-dynamic -Wl,--export=__heap_base -Wl,--export=__data_end -Wl,--export=wasi_thread_start -Wl,--export-table -Wl,--growable-table -Wl,--import-memory=false"
